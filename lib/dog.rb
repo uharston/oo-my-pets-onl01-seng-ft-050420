313af86375558ab23be6bbb678526ccd5ab75b12
Uriah Harston <uharston@gmail.com>
2020-05-19 20:08:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
-class Dog
-  # code goes here
+class Dog 
+
+  @@all = []
+
+  attr_reader :name
+
+  attr_accessor :owner, :mood
+
+  def initialize(name, owner=@owner) #initializes with a name and an Owner
+    @name = name #can't change the name because their is no setter after initialize
+    @owner = owner #can change the owner because of attr_accessor
+    @mood = "nervous" #initializes with nervous mood and can change it later
+  end
+
+  def self.all
+    @@all
+  end
 end
