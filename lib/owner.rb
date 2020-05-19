2dbf838a17067caef0361ea095c60ebeddd7f459
Uriah Harston <uharston@gmail.com>
2020-05-19 20:13:31 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,13 +1,20 @@
 class Owner
 
+  @@all = []
+
   attr_reader :name, :species
 
   def initialize(name)
     @name = name
     @species = "human"
+    @@all << self 
   end
 
   def say_species
     "I am a #{@species}."
   end
+
+  def self.all 
+    @@all 
+  end 
 end
