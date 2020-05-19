cd3ab35dc6243063e5a40ca45173197957fa3d63
Uriah Harston <uharston@gmail.com>
2020-05-19 20:03:37 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
 class Cat
-  # code goes here
+
+  @@all = []
+  
+  attr_reader :name 
+
+  attr_accessor :owner :mood 
+
+  def initialize(name, owner=@owner)
+    @name = name 
+    @owner = owner 
+    @mood = "nervous"
+  end 
+
+  def self.all 
+    @@all 
+  end
 end
